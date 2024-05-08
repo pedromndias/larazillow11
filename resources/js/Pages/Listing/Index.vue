f16b38ac0cc8e9a710ca2ef7a7d73dd23aced65b
Pedro Dias <pedromndias@gmail.com>
2024-05-08 09:53:23 +0200

    configure tailwind

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,15 +1,18 @@
 <template>
-  <div v-for="listing in listings" :key="listing.id">
-    <Link :href="`/listing/${listing.id}`">
-      <ListingAddress :listing="listing" />
-    </Link>
-  </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <Box v-for="listing in listings" :key="listing.id">
+            <Link :href="`/listing/${listing.id}`">
+                <ListingAddress :listing="listing" />
+            </Link>
+        </Box>
+    </div>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3'
-import ListingAddress from '../../Components/ListingAddress.vue';
-  defineProps({
-    listings: Array
-  })
+import { Link } from "@inertiajs/vue3";
+import ListingAddress from "../../Components/ListingAddress.vue";
+import Box from "../../Components/UI/Box.vue";
+defineProps({
+    listings: Array,
+});
 </script>
