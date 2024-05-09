598cbc8e8fbe5bd9413a38c0560a81862c0f8762
Pedro Dias <pedromndias@gmail.com>
2024-05-09 10:32:34 +0200

    register form

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\UserAccountController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
+
+Route::resource('user-account', UserAccountController::class)
+  ->only(['create']);
